bd259286f71bcec21dadabde3f817ed2dbf77878
Espartaco Palma <esparta@gmail.com>
2013-09-21 20:15:28 -0500

    Script for change automagically (almost) the markdown to Jekyll format

---
+++ libro/cf.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+import sys
+import os
+
+def main(argv):
+   if len(argv) < 2:
+     sys.stderr.write("Usage: {0}  <filename>\n".format(argv[0]))
+     return 2
+   filename = argv[1]
+   if not os.path.exists(filename):
+     sys.stderr.write("ERROR: file {filename} not found!\n"\
+                      .format(filename=filename))
+     return 1
+   justfilename, _ = os.path.splitext(filename)
+
+   with open(filename,"r+b") as f:
+      ## Get the title and remove the newline
+      title = f.readline().rstrip(os.linesep)
+      print(title)
+      if title in ("---", ""):
+        sys.stderr.write("Warning! This file appears to be already formatted\n")
+        return 1
+      ## skip the next line, should be ===
+      _ = f.readline()
+      ## create the head
+      head = \
+"""---
+layout: page
+title: {title}
+overview: true
+permalink: /libro/{filename}/
+---
+""".format(title=title,filename=justfilename)
+      #print(head)
+      content = f.read()
+      f.seek(0)
+      f.truncate()
+      f.write(head+content)
+      print("Finished transforming {0}".format(filename))
+
+if __name__ == "__main__":
+  sys.exit(main(sys.argv))
+
